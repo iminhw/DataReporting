b99f1caf2425033d6a46e4654830d27ceb72ebcb
凉伊 <iminhw@qq.com>
2022-03-23 11:04:18 +0800

    first commit

---
+++ src/test/java/com/cap/datareporting/DataReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.datareporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
