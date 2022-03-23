c4e6427532d960c9fe39bcd682f9d843a9bbce88
凉伊 <iminhw@qq.com>
2022-03-23 12:44:09 +0800

    first commit

--- src/main/java/com/cap/datareporting/controller/LoginController.java
+++ src/main/java/com/cap/datareporting/controller/LoginController.java
@@ -1,14 +1,12 @@
 package com.cap.datareporting.controller;
 
+import com.cap.datareporting.utils.ResultEntity;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * @program: blog01
@@ -21,26 +19,26 @@ public class LoginController {
 
     @RequestMapping(value = "/ajaxLogin", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String, Object> submitLogin(@RequestParam("username") String username,
-                                           @RequestParam("password") String password,
+    public ResultEntity submitLogin(@RequestParam("username") String username,
+                                    @RequestParam("password") String password,
 //                                           @RequestParam("url") String url,
-                                           @RequestParam("rememberMe") Boolean rememberMe) {
-        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+                                    @RequestParam("rememberMe") Boolean rememberMe) {
+        ResultEntity resultMap = new ResultEntity();
 //        if (!username.equals("") && !password.equals("")) return "";
         try {
             UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe);
             SecurityUtils.getSubject().login(token);
-            resultMap.put("status", 200);
-            resultMap.put("message", "登录成功");
+            resultMap.setStatus(200);
+            resultMap.setMsg("登录成功");
         } catch (UnknownAccountException e) {
-            resultMap.put("status", 500);
-            resultMap.put("message", "账号不存在！");
+            resultMap.setStatus(500);
+            resultMap.setMsg("账号不存在！");
         } catch (IncorrectCredentialsException e1) {
-            resultMap.put("status", 500);
-            resultMap.put("message", "密码错误！");
+            resultMap.setStatus(500);
+            resultMap.setMsg("密码错误！");
         } catch (Exception e) {
-            resultMap.put("status", 500);
-            resultMap.put("message", "账号不存在");
+            resultMap.setStatus(500);
+            resultMap.setMsg("账号不存在");
         }
         return resultMap;
     }
